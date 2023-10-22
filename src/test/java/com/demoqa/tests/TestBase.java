2bee47339f258010d62cdd9786a330c4d16071d0
Denis Bashkatov <dend3lion@gmail.com>
2023-10-23 02:37:46 +0300

    fix: added remote launch

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,10 +1,13 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.api.AuthorizationApi;
 import com.demoqa.api.BooksApi;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
     AuthorizationApi authorizationApi = new AuthorizationApi();
@@ -12,9 +15,19 @@ public class TestBase {
 
     @BeforeAll
     static void setup() {
-        RestAssured.baseURI = "https://demoqa.com";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        RestAssured.baseURI = System.getProperty("baseURI", "https://demoqa.com");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty("browserrSize", "2560x1440");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.pageLoadStrategy = "eager";
+
+        String selenoidUrl = System.getProperty("selenoidUrl", "selenoid.autotests.cloud");
+        Configuration.remote = "https://user1:1234@" + selenoidUrl + "/wd/hub";
+    }
+
+    @BeforeEach
+    void logAllure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
